6400c41ee42e50be2ff5210909b5b3f9546d4b76
greenbully4 <greenbull448@gmail.com>
2024-12-22 04:20:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
+SESSION_ID: 'HANSAMAL-MD=LUUUyIia#fSP8znG2AGzONh_pRo9sB0_lEOHGuyEibpFojwo9uJM',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '18763834999' : process.env.OWNER_NUMBER
 };
